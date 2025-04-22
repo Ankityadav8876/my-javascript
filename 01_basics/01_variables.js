0f10555e3ea501de0afe1b6f7192becfd8478303
Ankityadav8876 <ankitydv8876@gmail.com>
2025-04-22 19:16:56 +0000

    datatpes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,4 @@
+const accountId = 2468
+let accountEmail =" anitydv23@gmail.com"
+
+console.table([accountId,accountEmail]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all Js code as newer version
+ 
+// alert(5+5)  // we are using nodejs, not browser
+
+ // number => 2 to power 53
+ // bignit
+ // string => ""
+ // boolean => standalone value
+ // undefined =>
+    // symbol => unique
+ // object
+
+ console.log(typeof undefined); //undefined
+ console.log(typeof null); // object
+
+ 
+
+
+
+
+
+
+
+
